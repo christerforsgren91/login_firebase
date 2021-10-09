912af35fb3d1826e242358cf1c1065b466d8c517
Christer <christerforsgren91@gmail.com>
2021-10-09 19:42:51 +0200

    Add submit handling to sign up form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import SignUp from './components/SignUp'
+import { AuthProvider } from './context/AuthContext'
 
 const App = () => {
   return (
-    <Container
-      className='d-flex align-items-center justify-content-center'
-      style={{ minHeight: '100vh' }}
-    >
-      <div className='w-100' style={{ maxWidth: '400px' }}>
-        <SignUp />
-      </div>
-    </Container>
+    <AuthProvider>
+      <Container
+        className='d-flex align-items-center justify-content-center'
+        style={{ minHeight: '100vh' }}
+      >
+        <div className='w-100' style={{ maxWidth: '400px' }}>
+          <SignUp />
+        </div>
+      </Container>
+    </AuthProvider>
   )
 }
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,17 +1,40 @@
-import React, { useRef } from 'react'
-import { Card, Button, Form } from 'react-bootstrap'
+import React, { useRef, useState } from 'react'
+import { Card, Button, Form, Alert } from 'react-bootstrap'
+import { useAuth } from '../context/AuthContext'
 
 const SignUp = () => {
   const emailRef = useRef()
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
+  const { signup, currentUser } = useAuth()
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError('Lösenordet matchar inte')
+    }
+
+    try {
+      setError('')
+      setLoading(true)
+      await signup(emailRef.current.value, passwordRef.current.value)
+    } catch {
+      setError('Gick inte att registrera en användare')
+    }
+    setLoading(false)
+  }
 
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className='text-center mb-4'>Registrera</h2>
-          <Form>
+          {currentUser && currentUser.email}
+          {error && <Alert variant='danger'>{error}</Alert>}
+          <Form onSubmit={handleSubmit} >
             <Form.Group id='email'>
               <Form.Label>Email</Form.Label>
               <Form.Control type='email' ref={emailRef} required />
@@ -24,7 +47,7 @@ const SignUp = () => {
               <Form.Label>Bekräfta Lösenord</Form.Label>
               <Form.Control type='password' ref={passwordConfirmRef} required />
             </Form.Group>
-            <Button className='w-100 mt-2' type='submit'>
+            <Button disabled={loading} className='w-100 mt-2' type='submit'>
               Registrera
             </Button>
           </Form>
